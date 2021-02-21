affa39a4c0c5e4f42492430dbc4d19a8b1280bcf
Phantom <eropkinp@gmail.com>
2021-02-21 21:07:38 +0300

    Описание моделей и подготовка тестовых данных

---
+++ WpfMailSender/Data/TestData.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfMailSender.Models;
+
+namespace WpfMailSender.Data
+{
+    internal static class TestData
+    {
+        public static List<Server> Servers { get; } = Enumerable.Range(1, 5)
+            .Select(i => new Server
+            {
+                Id = i,
+                Address = $"smpt.server{i}.ru",
+                UseSSL = i % 2 == 0,
+                Login = "user@yandex.ru",
+                Password = "PassWord",
+                Description = $"This is server {i}",
+                Name = $"Server {i}"
+
+            }).ToList();
+
+        public static List<Sender> Senders { get; } = Enumerable.Range(1, 5)
+            .Select(i => new Sender
+            {
+                Id = i,
+                Name = $"Sender{i}",
+                Address = $"Sender{i}@server.ru",
+                Description = $"Mail from Sender{i}"
+
+            }).ToList();
+
+        public static List<Recipient> Recipients { get; } = Enumerable.Range(1, 5)
+            .Select(i => new Recipient
+            {
+                Id = i,
+                Name = $"Recipient{i}",
+                Address = $"Recipient{i}@server.ru",
+                Description = $"Mail for Recipient{i}"
+
+            }).ToList();
+
+        public static List<Message> Messages { get; } = Enumerable.Range(1, 5)
+            .Select(i => new Message
+            {
+                Id = i,
+                Tittle = $"Сообщение {i}",
+                Body = $"Текст сообщения {i}"
+
+            }).ToList();
+    }
+}
---
+++ WpfMailSender/Models/Sender.cs
@@ -0,0 +1,10 @@
+﻿namespace WpfMailSender.Models
+{
+    public class Sender
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Description { get; set; }
+    }
+}
