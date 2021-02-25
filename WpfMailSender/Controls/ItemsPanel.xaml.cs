db99e4ec64fcf4fe64ba9c188cfab61c0f491972
Phantom <eropkinp@gmail.com>
2021-02-25 16:53:12 +0300

    Модель представления

--- WpfMailSender/Controls/ItemsPanel.xaml.cs
+++ WpfMailSender/Controls/ItemsPanel.xaml.cs
@@ -1,30 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+
 
 namespace WpfMailSender.Controls
 {
     /// <summary>
     /// Interaction logic for ItemsPanel.xaml
     /// </summary>
-    public partial class ItemsPanel : UserControl
+    public partial class ItemsPanel
     {
-        public string Title { get => Header.Text; set => Header.Text = value; }
+        public static readonly DependencyProperty TitleProperty = 
+            DependencyProperty.Register(
+            nameof(Title),
+            typeof(string),
+            typeof(ItemsPanel),
+            //new PropertyMetadata(default(string)));
+            new PropertyMetadata("Название"));
 
-        public ItemsPanel()
+        public string Title
         {
-            InitializeComponent();
+            get => (string)GetValue(TitleProperty); 
+            set => SetValue(TitleProperty, value);
         }
+
+        public ItemsPanel() => InitializeComponent();
     }
 }
